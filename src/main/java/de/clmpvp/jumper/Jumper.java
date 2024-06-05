23eba7cff345d8c4f0dd68c2c06be22d05af2938
mstvsurvival <minecraftsurvivaltvvt@gmail.com>
2024-06-05 20:06:39 +0200

    Added prefix, noperm, use, console and world

--- src/main/java/de/clmpvp/jumper/Jumper.java
+++ src/main/java/de/clmpvp/jumper/Jumper.java
@@ -4,14 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Jumper extends JavaPlugin {
 
+    public static String prefix = "§7[§bJumpNRun§7] ";
+    public static String noperm = "§7[§bJumpNRun§7] §cDazu hast du keine Rechte";
+    public static String use = "§7[§bJumpNRun§7] Bitte benutze";
+    public static String console = "§7[§bJumpNRun§7] Nur Spieler können dies Ausführen";
+    public static String world = "JumpNRun";
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
